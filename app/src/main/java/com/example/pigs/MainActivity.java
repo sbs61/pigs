8ff10c78d3ccd294cd4c967203a378e8f3f4a1a9
ingisteinn <ingi.gudmunds@gmail.com>
2019-03-16 16:34:43 +0000

    GET fyrir exercise

--- app/src/main/java/com/example/pigs/MainActivity.java
+++ app/src/main/java/com/example/pigs/MainActivity.java
@@ -1,15 +1,37 @@
 package com.example.pigs;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.pigs.R;
+import com.example.pigs.controllers.ExerciseController;
+import com.example.pigs.entities.Exercise;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private ExerciseController exerciseController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        AsyncTask task = new FetchItemsTask();
+        task.execute();
+
         setContentView(R.layout.activity_main);
+    }
+
+    private class FetchItemsTask extends AsyncTask<Object, Void, String> {
+        @Override
+        protected String doInBackground(Object... params) {
+            return new ExerciseController().getExercises();
+        }
+
+        @Override
+        protected void onPostExecute(String items) {
+            System.out.println(items);
+        }
     }
 }
--- app/src/main/java/com/example/pigs/controllers/ExerciseController.java
+++ app/src/main/java/com/example/pigs/controllers/ExerciseController.java
@@ -1,4 +1,37 @@
 package com.example.pigs.controllers;
 
+import android.util.Log;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import com.example.pigs.MainActivity;
+import java.io.IOException;
+
 public class ExerciseController {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    public String getExercises() {
+        String url = "https://hugbun2.herokuapp.com/exercise";
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        String jsonString = null;
+
+        try {
+            Response res = client.newCall(request).execute();
+            if (res.isSuccessful()){
+                jsonString = res.body().string();
+                Log.e(TAG, res.toString());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Exception caught: ", e);
+        }
+
+        return jsonString;
+    }
+
+
 }
