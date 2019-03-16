8f0f3c68dbd01dea56dd5dfc3906d40a30b36b0c
Pattihrafn22 <35534176+Pattihrafn22@users.noreply.github.com>
2019-03-16 16:40:31 +0000

    hm

--- app/src/main/java/com/example/pigs/MainActivity.java
+++ app/src/main/java/com/example/pigs/MainActivity.java
@@ -13,24 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);
-        final int day = 25;
-        final int month2 = 3;
-        final int year2 = 2019;
-        CalendarView calendarView = (CalendarView) findViewById(R.id.CalendarView);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        setContentView(R.layout.activity_create_workout);
 
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getApplicationContext(), "" + dayOfMonth + "." + (month+1) + "." + year, 5).show();
-                TextView foundWorkout = (TextView) findViewById(R.id.foundWorkout);
-                if(dayOfMonth == day && (month+1) == month2 && year == year2) {
-                    foundWorkout.setText("Bench Press");
-                }
-                else {
-                    foundWorkout.setText("No Workout Found");
-                }
-            }
-        });
     }
 }
