865ebb3ba518341c5b1860aca66d4020091426a2
sbs61@hi.is <sbs61@hi.is>
2019-03-16 17:05:59 +0000

    navigation

--- app/src/main/java/com/example/pigs/MainActivity.java
+++ app/src/main/java/com/example/pigs/MainActivity.java
@@ -1,36 +1,64 @@
 package com.example.pigs;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.CalendarView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.example.pigs.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);
-        final int day = 25;
-        final int month2 = 3;
-        final int year2 = 2019;
-        CalendarView calendarView = (CalendarView) findViewById(R.id.CalendarView);
-        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        setContentView(R.layout.activity_create_exercise);
 
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getApplicationContext(), "" + dayOfMonth + "." + (month+1) + "." + year, 5).show();
-                TextView foundWorkout = (TextView) findViewById(R.id.foundWorkout);
-                if(dayOfMonth == day && (month+1) == month2 && year == year2) {
-                    foundWorkout.setText("Bench Press");
-                }
-                else {
-                    foundWorkout.setText("No Workout Found");
-                }
-            }
-        });
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/pigs/activities/workout/ScheduleActivity.java
+++ app/src/main/java/com/example/pigs/activities/workout/ScheduleActivity.java
@@ -1,4 +1,36 @@
 package com.example.pigs.activities.workout;
 
-public class ScheduleActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.pigs.R;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_exercise);
+        final int day = 25;
+        final int month2 = 3;
+        final int year2 = 2019;
+        CalendarView calendarView = (CalendarView) findViewById(R.id.CalendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), "" + dayOfMonth + "." + (month+1) + "." + year, 5).show();
+                TextView foundWorkout = (TextView) findViewById(R.id.foundWorkout);
+                if(dayOfMonth == day && (month+1) == month2 && year == year2) {
+                    foundWorkout.setText("Bench Press");
+                }
+                else {
+                    foundWorkout.setText("No Workout Found");
+                }
+            }
+        });
+    }
 }
