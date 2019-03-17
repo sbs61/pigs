013220a7bee3fe3a3a054240c3b148cb3e355411
ingisteinn <ingi.gudmunds@gmail.com>
2019-03-17 14:23:44 +0000

    wow

--- app/src/main/java/com/example/pigs/activities/exercise/ExercisesActivity.java
+++ app/src/main/java/com/example/pigs/activities/exercise/ExercisesActivity.java
@@ -1,4 +1,36 @@
 package com.example.pigs.activities.exercise;
 
-public class ExercisesActivity {
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.pigs.MainActivity;
+import com.example.pigs.R;
+import com.example.pigs.controllers.ExerciseController;
+import com.example.pigs.entities.Exercise;
+
+public class ExercisesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AsyncTask task = new ExercisesActivity.FetchItemsTask();
+        task.execute();
+
+        setContentView(R.layout.activity_main);
+    }
+
+    private class FetchItemsTask extends AsyncTask<Object, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(Object... params) {
+            Exercise ex = new Exercise(2L, "Bench press", "Chest");
+            return new ExerciseController().createExercise(ex);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean items) {
+            System.out.println(items);
+        }
+    }
 }
