6f37ac98e856e7138c9c2f357ee5ac3d9703138b
Pattihrafn22 <35534176+Pattihrafn22@users.noreply.github.com>
2019-03-17 21:06:18 +0000

    CreateWorkoutActivity

--- app/src/main/java/com/example/pigs/MainActivity.java
+++ app/src/main/java/com/example/pigs/MainActivity.java
@@ -7,23 +7,39 @@ import android.os.Bundle;
 import android.widget.CalendarView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 import com.example.pigs.R;
 import com.example.pigs.activities.exercise.ExercisesActivity;
 import com.example.pigs.controllers.ExerciseController;
 
+import com.example.pigs.activities.workout.CreateWorkoutActivity;
+
 public class MainActivity extends AppCompatActivity {
     private ExerciseController exerciseController;
+    private CreateWorkoutActivity createWorkoutActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AsyncTask task = new FetchItemsTask();
-        task.execute();
-        setContentView(R.layout.activity_create_workout);
-        Intent i = new Intent(MainActivity.this, ExercisesActivity.class);
-        startActivity(i);
-   }
 
+        Log.d("test", "testing testing");
+
+        Intent intent = new Intent(MainActivity.this, CreateWorkoutActivity.class);
+        MainActivity.this.startActivity(intent);
+        //setContentView(R.layout.activity_create_workout);
+
+
+    }
 
 }
